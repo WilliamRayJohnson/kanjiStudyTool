a35eb72b6d58d4331df6a08276a188a2d43e5d32
William Ray Johnson <willrayjohnson@gmail.com>
2017-12-12 23:34:36 -0600

    Added more style

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
         <meta charset="utf-8" />
         
         <link rel="stylesheet" type="text/css" href="index.css" />
+        <style>
+        #feedback { font-size: 1.4em; }
+        #selectable .ui-selecting { background: #FECA40; }
+        #selectable .ui-selected { background: #F39814; color: white; }
+        #selectable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
+        #selectable li { margin: 3px; padding: 0.4em; font-size: 1.4em; height: 18px; }
+        </style>
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
         <script>
