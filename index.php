ff85546449e461fb9b865d0062ab217fb83eaf47
William Ray Johnson <willrayjohnson@gmail.com>
2017-12-12 23:24:10 -0600

    Added test question to index

--- index.php
+++ index.php
@@ -6,13 +6,21 @@
         <meta charset="utf-8" />
         
         <link rel="stylesheet" type="text/css" href="index.css" />
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script>
+        $( function() {
+            $( "#selectable" ).selectable();
+        } );
+        </script>
     </head>
 
     <body>
         <?php
             include 'src/Question.php';
             
-            
+            $aQuestion = new Question("Test", array("test"));
+            echo $aQuestion->getFormattedQuestion();
         ?>
     </body>
 </html>
