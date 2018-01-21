6cab4df5eb89384f538e50a4b56aea886b524616
William Ray Johnson <willrayjohnson@gmail.com>
2018-01-21 14:04:04 -0600

    Added sql script for adding kanji to the database

---
+++ sql/INSERT_KANJI.sql
@@ -0,0 +1,16 @@
+/* This script will insert kanji into the kanji table */
+
+INSERT INTO 'kanji' ('id', 'kanji', 'source_id')
+    VALUES (NULL, "一", 1);
+
+INSERT INTO 'kanji' ('id', 'kanji', 'source_id')
+    VALUES (NULL, "二", 1);
+    
+INSERT INTO 'kanji' ('id', 'kanji', 'source_id')
+    VALUES (NULL, "三", 1);
+    
+INSERT INTO 'kanji' ('id', 'kanji', 'source_id')
+    VALUES (NULL, "四", 1);
+    
+INSERT INTO 'kanji' ('id', 'kanji', 'source_id')
+    VALUES (NULL, "五", 1);
