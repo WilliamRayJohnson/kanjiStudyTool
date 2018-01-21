363215565dafc59d8fe55ac0237365ac896d3b55
William Ray Johnson <willrayjohnson@gmail.com>
2018-01-21 14:52:31 -0600

    Added more insert scripts

---
+++ sql/INSERT_SOURCES.sql
@@ -0,0 +1,16 @@
+/* Inserts sources into the kanji_source table */
+
+INSERT INTO `kanji_source` (`id`, `source`)
+    VALUES (NULL, "Genki I: Ch. 1");
+    
+INSERT INTO `kanji_source` (`id`, `source`)
+    VALUES (NULL, "Genki I: Ch. 2");
+    
+INSERT INTO `kanji_source` (`id`, `source`)
+    VALUES (NULL, "Genki I: Ch. 3");
+    
+INSERT INTO `kanji_source` (`id`, `source`)
+    VALUES (NULL, "Genki I: Ch. 4");
+    
+INSERT INTO `kanji_source` (`id`, `source`)
+    VALUES (NULL, "Genki I: Ch. 5");
---
+++ sql/INSERT_WORDS.sql
@@ -0,0 +1,16 @@
+/* Inserts words into the word table */
+
+INSERT INTO `word` (`id`, `word`, `reading`)
+    VALUE (NULL, "一", "いち");
+    
+INSERT INTO `word` (`id`, `word`, `reading`)
+    VALUE (NULL, "二", "に");
+    
+INSERT INTO `word` (`id`, `word`, `reading`)
+    VALUE (NULL, "三", "さん");
+    
+INSERT INTO `word` (`id`, `word`, `reading`)
+    VALUE (NULL, "四", "よん");
+    
+INSERT INTO `word` (`id`, `word`, `reading`)
+    VALUE (NULL, "五", "ご");
