a806f8ef6ffa1899696487588e81a2c4b69df872
William Ray Johnson <willrayjohnson@gmail.com>
2018-01-25 21:24:41 -0600

    Updated CREATE_TABLES to link foreign keys

---
+++ sql/CREATE_KANJI_STUDIER_DB.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE kanji_studier
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
--- sql/CREATE_TABLES.sql
+++ sql/CREATE_TABLES.sql
@@ -1,23 +1,29 @@
 /*These are the set of tables to be used in the Kanji Studier Project*/
 
+CREATE TABLE kanji_source (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    source VARCHAR(40));
+
 CREATE TABLE kanji (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    kanji VARCHAR(1),
-    source_id INT);
+    kanji VARCHAR(1) UNIQUE KEY,
+    FOREIGN KEY (source_id)
+        REFERENCES kanji_source(id)
+        ON UPDATE CASCADE ON DELETE RESTRICT);
 
 CREATE TABLE word (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    word VARCHAR(10),
+    word VARCHAR(10) UNIQUE KEY,
     reading VARCHAR(20));
 
 CREATE TABLE kanji_in_word (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    word_id INT,
-    kanji_id INT);
-
-CREATE TABLE kanji_source (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    source VARCHAR(40));
+    FOREIGN KEY (word_id)
+        REFERENCES word(id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (kanji_id)
+        REFERENCES kanji(id)
+        ON UPDATE CASCADE ON DELETE CASCADE);
 
 CREATE TABLE student (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -27,8 +33,12 @@ CREATE TABLE student (
 
 CREATE TABLE student_kanji (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    stduent_id INT,
-    kanji_id INT,
+    FOREIGN KEY (student_id)
+        REFERENCES student(id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (kanji_id)
+        REFERENCES kanji(id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
     retention_score FLOAT,
     correct_response_count INT,
     incorrect_response_count INT,
