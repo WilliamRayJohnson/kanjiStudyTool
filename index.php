d3363f80912bd62ed7f28f367f8d7a016a3c658d
William Ray Johnson <willrayjohnson@gmail.com>
2018-01-30 15:19:30 -0600

    Added some basic MySql interaction

--- index.php
+++ index.php
@@ -18,10 +18,27 @@
     <body>
         <div align="center">
         <?php
-            include 'src/Question.php';
+            $dbInfo = include('config/config.php');
+            $db = mysqli_connect($dbInfo['DB_SERVER'], $dbInfo['DB_USERNAME'], $dbInfo['DB_PASSWORD'], $dbInfo['DB_DATABASE']);
+            mysqli_set_charset($db, "utf8");
             
-            $aQuestion = new Question("Test", array("answer one", "answer two"));
-            echo $aQuestion->getFormattedQuestion();
+            if (!$db) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            
+            $sql = "SELECT * FROM kanji";
+            $result = mysqli_query($db, $sql);
+            
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<p>" . $row["kanji"] . "</p>";
+                }
+            }
+            else {
+                echo "0 results";
+            }
+            
+            mysqli_close($db);
         ?>
         </div>
     </body>
