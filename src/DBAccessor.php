d6b3f1b79538cd88b74e48f7b7a537a17386c5e4
William Ray Johnson <willrayjohnson@gmail.com>
2018-01-30 20:26:10 -0600

    Revert "Moved sql query to file"
    This reverts commit 524131294036801a4f994dfa48713d53c1b6f748.

--- src/DBAccessor.php
+++ src/DBAccessor.php
@@ -17,10 +17,9 @@
                 die("Connection failed: " . mysqli_connect_error());
             }
             
-            $file = fopen('sql/GET_ALL_KANJI.sql', "r+");
-            $sql = fread($file, filesize($file));
-            
+            $sql = "SELECT * FROM kanji";
             $result = mysqli_query($db, $sql);
+            
             if (mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_assoc($result)) {
                     echo "<p>" . $row["kanji"] . "</p>";
