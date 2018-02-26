98e279c5ccd9017508b67e5a4b4b34b03dc66544
William Ray Johnson <willrayjohnson@gmail.com>
2018-02-25 18:11:13 -0600

    Added functionality for addQuestion and isQuizComplete

--- src/Quizer.php
+++ src/Quizer.php
@@ -5,6 +5,7 @@
         
         function __construct() {
             $this->questions = array();
+            $this->currentQuestionIndex = 0;
         }
         
         function questionCount() {
@@ -12,7 +13,27 @@
         }
         
         function isQuizComplete() {
-            return(false);
+            $quizComplete = true;
+            $qCount = count($this->questions);
+            $currentQ = 0;
+            if($qCount > 0) {
+                while($quizComplete and $currentQ < $qCount) {
+                    $quizComplete = $this->questions[$currentQ]->hasCorrectAnswer();
+                    $currentQ++;
+                }
+            }
+            else {
+                $quizComplete = false;
+            }
+            return($quizComplete);
+        }
+        
+        function addQuestion($question, $answers, $correctResponse) {
+            array_push($this->questions, new Question(count($this->questions), $question, $answers, $correctResponse));
+        }
+        
+        function getCurrentQuestion() {
+            return($this->questions[$this->currentQuestionIndex]);
         }
     }
 ?>
