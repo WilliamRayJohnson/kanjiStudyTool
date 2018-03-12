63de941019b683dedfaddaa11ef370e71304f511
William Ray Johnson <willrayjohnson@gmail.com>
2018-03-12 13:53:59 -0500

    Implemented basic retention score calculation

--- src/QuizResultsProcessor.php
+++ src/QuizResultsProcessor.php
@@ -1,7 +1,30 @@
 <?php
+define("QUIZ_MAX_CHANGE", "0.1");
+define("PAST_MAX_CHANGE", "0.02");
+
 class QuizResultsProcessor {
+    const QUIZ_MAX_CHANGE = 0.1;
+    const PAST_MAX_CHANGE = 0.02;
+    
     function calcBasicRetentionScore($pastCR, $pastIR, $quizCR, $quizIR, $originalRetentionScore) {
-        return $originalRetentionScore;
+        $quizRatio;
+        $pastRatio;
+        $newRetentionScore;
+        
+        if($pastCR > $pastIR) 
+            $pastRatio = (float)$pastIR/(float)$pastCR;
+        else if($pastIR >= $pastCR)
+            $pastRatio = -(float)$pastCR/(float)$pastIR;
+        if($quizCR > $quizIR) 
+            $quizRatio = (float)$quizIR/(float)$quizCR;
+        else if($quizIR >= $quizCR)
+            $quizRatio = -(float)$quizCR/(float)$quizIR;
+        
+        $newRetentionScore = $originalRetentionScore + (QUIZ_MAX_CHANGE * $quizRatio) + (PAST_MAX_CHANGE * $pastRatio);
+        
+        if($newRetentionScore > 1.0)
+            $newRetentionScore = 1.0;
+        return $newRetentionScore;
     }
 }
 
