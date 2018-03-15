cf4b34165a1e9968acb37b2f84309434bba6013e
William Ray Johnson <willrayjohnson@gmail.com>
2018-03-15 15:26:46 -0500

    Created the beginnings of the site, involved lots of CSS changes and the additon of a few template pages

--- web/index.php
+++ web/index.php
@@ -7,13 +7,22 @@
         
         <link rel="stylesheet" type="text/css" href="index.css" />
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     </head>
 
     <body>
-        <div align="center">
-            <a href="QuizPage.php">Sample Quiz</a><br>
-            <a href="SessionTest.php">Session Test</a>
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li class="dropdown" style="float:right">
+                <a href="javascript:void(0)" class="dropbtn">wrj6844</a>
+                <div class="dropdown-content">
+                    <a href="profile.html">Profile</a>
+                    <a href="#">Logout</a>
+                </div>
+            </li>
+        </ul>
+        
+        <div class="content">
+            <a class="content-button" href="quiz.html">Take a Quiz</a>
         </div>
     </body>
 </html>
