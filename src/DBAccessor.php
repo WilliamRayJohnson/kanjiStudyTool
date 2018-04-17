df25d4ec14ca3080fff04166085dd81c23e4caa8
William Ray Johnson <willrayjohnson@gmail.com>
2018-04-17 16:38:07 -0500

    Modified getQuizAnswers query to only return DISTINCT words

--- src/DBAccessor.php
+++ src/DBAccessor.php
@@ -270,19 +270,21 @@
             if (!$db)
                 die("Connection failed: " . mysqli_connect_error());
 
-            $stmt = $db->prepare("SELECT w.word, w.reading, FLOOR(100 + RAND()*100) AS weight
-                                    FROM word w
-                                    JOIN kanji_in_word kiw ON kiw.word_id = w.id
-                                    JOIN kanji k ON k.id = kiw.kanji_id
-                                    WHERE k.kanji = ? AND NOT w.word = ?
-                                UNION
-                                SELECT w.word, w.reading, FLOOR(200 + RAND()*100) AS weight
-                                    FROM word w
-                                    JOIN kanji_in_word kiw ON kiw.word_id = w.id
-                                    JOIN kanji k ON k.id = kiw.kanji_id
-                                    WHERE k.source_id = (SELECT source_id FROM kanji WHERE kanji=?) AND NOT w.word = ?
-                                ORDER BY weight
-                                LIMIT ?");
+            $stmt = $db->prepare("SELECT DISTINCT a.word, a.reading
+                                    FROM (
+                                        SELECT w.word, w.reading, FLOOR(100 + RAND()*100) AS weight
+                                            FROM word w
+                                            JOIN kanji_in_word kiw ON kiw.word_id = w.id
+                                            JOIN kanji k ON k.id = kiw.kanji_id
+                                            WHERE k.kanji = ? AND NOT w.word = ?
+                                        UNION
+                                        SELECT w.word, w.reading, FLOOR(200 + RAND()*100) AS weight
+                                            FROM word w
+                                            JOIN kanji_in_word kiw ON kiw.word_id = w.id
+                                            JOIN kanji k ON k.id = kiw.kanji_id
+                                            WHERE k.source_id = (SELECT source_id FROM kanji WHERE kanji=?) AND NOT w.word = ?
+                                        ORDER BY weight ) AS a
+                                    LIMIT ?");
             $stmt->bind_param("ssssi", $kanji, $word, $kanji, $word, $answerCount);
             $stmt->execute();
             $results = $stmt->get_result();
